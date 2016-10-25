b83555fcf37fb210d460c73945b2bf16f2682483
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2016-10-25 10:55:35 -0200

    Some migrations access the database and require data to be present. This is wrong. For now, we’ll just skip the migrations that does that.
    Another issue is that the production schema is somehow different from the migrations. In order to fix that, some migration code was commented or comments were removed.

--- db/migrate/20150901144752_devise_create_users.rb
+++ db/migrate/20150901144752_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -30,13 +30,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
+      # t.string :name
       t.datetime :deleted_at
 
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/migrate/20160111183519_add_plugin_relation_id_to_materials.rb
+++ db/migrate/20160111183519_add_plugin_relation_id_to_materials.rb
@@ -2,8 +2,19 @@ class AddPluginRelationIdToMaterials < ActiveRecord::Migration
   def change
     add_reference :materials, :plugin_relation, index: true, foreign_key: true
 
-    cycle = Cycle.find 'seguranca-publica'
+    begin
+      cycle = Cycle.find 'seguranca-publica'
+    rescue ActiveRecord::RecordNotFound
+      puts "Cycle seguranca-publica not found. Skipping."
+      return
+    end
+
     pr = cycle.plugin_relations.find_by_slug 'biblioteca'
+
+    unless pr
+      puts "'biblioteca' plugin relation not found. Skipping."
+      return
+    end
 
     Material.find_each do |m|
       m.update_column(:plugin_relation_id, pr.id)
