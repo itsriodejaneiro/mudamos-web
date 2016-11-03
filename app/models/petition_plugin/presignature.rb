80575c29c39297a86f4cc9ed9f5b05546bb3f441
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2016-11-03 17:00:58 -0200

    Annotate the models.

--- app/models/petition_plugin/presignature.rb
+++ app/models/petition_plugin/presignature.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: petition_plugin_presignatures
+#
+#  id                 :integer          not null, primary key
+#  user_id            :integer          not null
+#  plugin_relation_id :integer          not null
+#  deleted_at         :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class PetitionPlugin::Presignature < ActiveRecord::Base
   acts_as_paranoid
 
--- spec/models/petition_plugin/presignature_spec.rb
+++ spec/models/petition_plugin/presignature_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: petition_plugin_presignatures
+#
+#  id                 :integer          not null, primary key
+#  user_id            :integer          not null
+#  plugin_relation_id :integer          not null
+#  deleted_at         :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require "rails_helper"
 
 RSpec.describe PetitionPlugin::Presignature do
