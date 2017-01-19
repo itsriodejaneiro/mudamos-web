bb679122c9484046ffc02ee557bf2e024ba50d7e
Erick <erickzanardoo@gmail.com>
2017-01-19 17:21:56 -0200

    reverting unnecessary changes

--- app/controllers/cycles/plugin_relations/petitions_controller.rb
+++ app/controllers/cycles/plugin_relations/petitions_controller.rb
@@ -20,11 +20,6 @@ class Cycles::PluginRelations::PetitionsController < ApplicationController
     head :ok
   end
 
-  helper_method :past_versions
-  def past_versions
-    PetitionPlugin::DetailRepository.new.past_versions_desc(@petition)
-  end
-
   private
 
   def ensure_user
