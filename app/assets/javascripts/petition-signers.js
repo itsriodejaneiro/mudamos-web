ecbcec1a3cccfc7236d3e56928ec483d57fe9691
Erick <erickzanardoo@gmail.com>
2017-01-30 15:32:52 -0200

    Prototyping the tablet widget and fixing the side bar background css

--- app/assets/javascripts/petition-signers.js
+++ app/assets/javascripts/petition-signers.js
@@ -1,11 +1,52 @@
 (function($) {
+
+  var buildPlugin = function(petitionId, apiClient, $element, addRow) {
+    var refreshList = function() {
+      // TODO this is mocked right now
+      apiClient.getPetitionSigners(petitionId, "2016-01-01T00:00:00", "2017-01-30T23:59:59")
+        .then(function(response) {
+
+          var signers = response.signers;
+          for (var i = 0; i < Math.min(signers.length, 4); i++) {
+            var signer = signers[i];
+            addRow($element, signer);
+          }
+        });
+    };
+
+    refreshList();
+  }
+
+  $.fn.petitionSignersTablet = function(petitionId, apiClient) {
+    $(this).each(function(idx, element) {
+      var $element = $(element);
+
+      $element.append("<div class='signers-tablet'><ul class='list-unstyled list-inline'></ul></div>");
+
+      var addRow =  function($element, userInfo) {
+        var $row = $("<li></li>");
+
+        var $img = $("<div class='pull-left'><img /></div>");
+        // TODO this is mocked while we don't have the user photos
+        $img.find("img").attr("src", "https://robohash.org/" + userInfo.name);
+        $row.append($img);
+
+        $element.find("ul").append($row);
+      };
+
+      buildPlugin(petitionId, apiClient, $element, addRow);
+    });
+
+    return this;
+  };
+
   $.fn.petitionSignersDesktop = function(petitionId, apiClient) {
     $(this).each(function(idx, element) {
       var $element = $(element);
 
       $element.append("<div class='signers-desktop'><ul class='list-unstyled'></ul></div>");
      
-      var addRow = function(userInfo) {
+      var addRow = function($element, userInfo) {
         var $row = $("<li></li>");
 
         var $img = $("<div class='pull-left'><img /></div>");
@@ -25,20 +66,7 @@
         $element.find("ul").append($row);
       }
 
-      var refreshList = function() {
-        // TODO this is mocked right now
-        apiClient.getPetitionSigners(petitionId, "2016-01-01T00:00:00", "2017-01-30T23:59:59")
-          .then(function(response) {
-
-            var signers = response.signers;
-            for (var i = 0; i < signers.length; i++) {
-              var signer = signers[i];
-              addRow(signer);
-            }
-          });
-      };
-
-      refreshList();
+      buildPlugin(petitionId, apiClient, $element, addRow);
     });
 
     return this;
