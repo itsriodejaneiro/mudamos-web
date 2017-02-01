4479019a85adf7594aa90ed75fa0d07eecf759e8
Erick <erickzanardoo@gmail.com>
2017-02-01 13:27:11 -0200

    adding user service

--- app/controllers/partners_api/users_controller.rb
+++ app/controllers/partners_api/users_controller.rb
@@ -1,11 +1,20 @@
 class PartnersApi::UsersController < PartnersApi::ApplicationController
+
+  attr_reader :user_service
+
+  def initialize(user_service: UserService.new)
+    @user_service = user_service
+  end
+
   def create
     user_params = params.permit(:email, :name, :encrypted_password)
-
-    user = User.new(name: user_params["name"], email: user_params["email"], password: user_params["encrypted_password"])
+    user = user_service.create_user_with_password(
+      email: user_params["email"],
+      name: user_params["name"],
+      encrypted_password: user_params["encrypted_password"]
+    )
     
-    if user.save
-      user.update_attribute :encrypted_password, user_params["encrypted_password"]
+    if user.valid?
       head 204
     else
       render json: user.errors, status: 422
