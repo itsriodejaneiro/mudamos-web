4670f02c5bf71cdb7c96e4f6b2e76ba21ab054b3
Erick <erickzanardoo@gmail.com>
2017-02-06 14:15:07 -0200

    improving blockquote

--- lib/pdf/elements/blockquote.rb
+++ lib/pdf/elements/blockquote.rb
@@ -1,6 +1,7 @@
 class Pdf::Elements::Blockquote
   def self.render(pdf, element)
-    # TODO we should render a gray background behind this
-    pdf.text element.css("p").inner_html, inline_format: true
+    pdf.indent 20 do
+      pdf.text element.css("p").inner_html, inline_format: true
+    end
   end
 end
