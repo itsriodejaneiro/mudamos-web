37a160fe259911ecd4f4b509e77cc23220e105e7
Erick <erickzanardoo@gmail.com>
2017-02-07 14:03:39 -0200

    PR suggestions

--- app/helpers/mobile_app_helper.rb
+++ app/helpers/mobile_app_helper.rb
@@ -5,9 +5,10 @@ module MobileAppHelper
     # For more information check https://developer.apple.com/library/content/documentation/AppleApplications/Reference/SafariWebContent/PromotingAppswithAppBanners/PromotingAppswithAppBanners.html
     return false if browser.platform.ios? && browser.safari?
 
-    cookie = cookies[:smart_banner_dismiss]
+    cookie_value = cookies[:smart_banner_dismiss]
+    banner_closed_at = Time.at(cookie_value ? cookie_value.to_i : 0)
 
-    (browser.device.mobile? || browser.device.tablet?) && (cookie.nil? || (Time.now >= Time.at(cookie.to_i / 1000) + 15.days))
+    (browser.device.mobile? || browser.device.tablet?) && Time.current >= banner_closed_at + 15.days
   end
 
   def android_store_page
