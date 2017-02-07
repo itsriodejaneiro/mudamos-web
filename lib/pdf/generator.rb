61995893d286c79164c1a10bf3e24c3b460eb2f7
Erick <erickzanardoo@gmail.com>
2017-02-07 16:23:20 -0200

    PR suggestions

--- lib/pdf/generator.rb
+++ lib/pdf/generator.rb
@@ -4,8 +4,23 @@ class Pdf::Generator
 
   def from_markdown(text)
     pdf = Prawn::Document.new
-    html_document = Nokogiri::HTML(markdown(text))
 
+    setup pdf
+    
+    pdf.repeat(:all) do
+      render_header pdf
+      render_border pdf
+      render_footer pdf
+    end
+
+    render_body pdf, text
+
+    pdf.render
+  end
+
+  private
+
+  def setup(pdf)
     font_path = Rails.root.join("app", "assets", "fonts")
     pdf.font_families.update(
       "Roboto" => {
@@ -17,36 +32,43 @@ class Pdf::Generator
     )
 
     pdf.font "Roboto"
+  end
 
-    pdf.repeat(:all) do
-      y_position = pdf.cursor
-      pdf.text_box "Projeto de iniciativa popular", size: 12, style: :bold, at: [70, pdf.cursor]
-      pdf.move_down 15
-      pdf.text_box "Ficha limpa", size: 12, style: :bold, at: [70, pdf.cursor]
+  def render_header(pdf)
+    y_position = pdf.cursor
+    pdf.text_box "Projeto de iniciativa popular", size: 12, style: :bold, at: [70, pdf.cursor]
+    pdf.move_down 15
+    pdf.text_box "Ficha limpa", size: 12, style: :bold, at: [70, pdf.cursor]
 
-      pdf.move_down 30
-      pdf.text_box "Ajude a acabar com a corrupção eleitoral no Brasil", size: 10, at: [70, pdf.cursor]
+    pdf.move_down 30
+    pdf.text_box "Ajude a acabar com a corrupção eleitoral no Brasil", size: 10, at: [70, pdf.cursor]
 
-      pdf.move_down 30
+    pdf.move_down 30
 
-      pdf.image Rails.root.join("app", "assets", "images", "logo_pdf.png").to_s, width: 70, height: 70, at: [0, y_position + 5]
+    pdf.image Rails.root.join("app", "assets", "images", "logo_pdf.png").to_s, width: 70, height: 70, at: [0, y_position + 5]
+  end
 
-      pdf.rectangle [0, pdf.cursor], 540, 585 
-      pdf.stroke
+  def render_border(pdf)
+    pdf.rectangle [0, pdf.cursor], 540, 585 
+    pdf.stroke
+  end
 
-      image_x = 230
-      image_y = 75 
+  def render_footer(pdf)
+    image_x = 230
+    image_y = 75 
 
-      pdf.rectangle [image_x - 10, image_y + 5], 90, 40 
-      pdf.fill_color "ffffff"
-      pdf.fill
-      pdf.fill_color "000000"
+    pdf.rectangle [image_x - 10, image_y + 5], 90, 40 
+    pdf.fill_color "ffffff"
+    pdf.fill
+    pdf.fill_color "000000"
 
-      pdf.image Rails.root.join("app", "assets", "images", "logo_pdf_horizontal.png").to_s, width: 70, at: [image_x, image_y]
-      pdf.text_box "Esse é um arquivo assinado digitalmente.", size: 12, at: [image_x - 80, image_y - 45]
-      pdf.text_box "Você pode verificar sua autenticidade acessando <u><link href='https://www.mudamos.org/'>https://www.mudamos.org/</link></u>", size: 12, at: [image_x - 180, image_y - 60], inline_format: true
-    end
+    pdf.image Rails.root.join("app", "assets", "images", "logo_pdf_horizontal.png").to_s, width: 70, at: [image_x, image_y]
+    pdf.text_box "Esse é um arquivo assinado digitalmente.", size: 12, at: [image_x - 80, image_y - 45]
+    pdf.text_box "Você pode verificar sua autenticidade acessando <u><link href='https://www.mudamos.org/'>https://www.mudamos.org/</link></u>", size: 12, at: [image_x - 180, image_y - 60], inline_format: true
+  end
 
+  def render_body(pdf, text)
+    html_document = Nokogiri::HTML(markdown(text))
     pdf.span(pdf.cursor, position: :left) do
       pdf.bounding_box([20, pdf.cursor - 30], width: 505, height: 530) do
         html_document.css("body").children.each do |element|
@@ -63,7 +85,5 @@ class Pdf::Generator
         end
       end
     end
-
-    pdf.render
   end
 end
