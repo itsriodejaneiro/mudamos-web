2577779f444be2a209e58f135becb280764e1f30
Erick <erickzanardoo@gmail.com>
2017-02-16 14:41:43 -0200

    removing mock

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,25 +1,7 @@
 class SignaturesController < ApplicationController
 
   def show
-    #@signature = signature_service.fetch_signature_status(params[:id])
-    @signature = valid_signature
-  end
-
-  private
-
-  #mocks
-  SignatureStatus = Struct.new(:petition_name, :petition_page_url, :blockchain_updated_at, :updated_at, :user_name)
-  def valid_signature
-    SignatureStatus.new(
-      "Petição bla",
-      "http://localhost:3000/temas/help-mario-save-the-princess",
-      Time.now,
-      Time.now,
-      "Davy Jones"
-    )
-  end
-
-  def invalid_signature
+    @signature = signature_service.fetch_signature_status(params[:id])
   end
 
   private
