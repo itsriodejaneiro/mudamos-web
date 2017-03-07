eb1aa374a6e4a6df8eaca33a86f8428667142fe2
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-03-07 13:43:07 -0300

    Added the pre signature email

---
+++ app/mailers/petition_plugin/pre_signature_mailer.rb
@@ -0,0 +1,25 @@
+module PetitionPlugin
+  class PreSignatureMailer < ApplicationMailer
+    layout false
+
+    def just_signed(user:, phase:, user_password: nil)
+      @user = user
+      @phase = phase
+      @user_password = user_password
+
+      mail to: @user.email, subject: I18n.t(:subject, scope: %i(mailer pre_signature))
+    end
+
+    private
+
+    helper do
+      def android_store_page
+        Rails.application.secrets.mobile_app["store_page"]["android"]
+      end
+
+      def apple_store_page
+        Rails.application.secrets.mobile_app["store_page"]["ios"]
+      end
+    end
+  end
+end
