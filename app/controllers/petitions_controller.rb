1dbb167fd1a62142f8cb2caf955c3e4cddd569b7
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-03-17 16:56:36 -0300

    New pdf verification design

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,10 +1,16 @@
 class PetitionsController < ApplicationController
+  before_action :set_page_title, only: %i(verify)
+
   def verify
   end
 
   def signatures
     @petition = petition_repository.find_by_id!(params[:petition_id])
     @signatures = petition_service.fetch_petition_signatures(params[:petition_id])
+  end
+
+  def use_simple_header?
+    params[:action] == "verify"
   end
 
   private
@@ -15,5 +21,11 @@ class PetitionsController < ApplicationController
 
   def petition_repository
     @petition_repository ||= PetitionPlugin::DetailRepository.new
+  end
+
+  def set_page_title
+    @page_title = case params[:action]
+                  when "verify" then "Validar documento"
+                  end
   end
 end
