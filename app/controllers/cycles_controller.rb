e07452d46f06b9368faf367440d9e8a04f74617c
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-03-27 17:08:07 -0300

    Add tools block mock design

--- app/controllers/cycles_controller.rb
+++ app/controllers/cycles_controller.rb
@@ -1,4 +1,10 @@
 class CyclesController < ApplicationController
+  before_action :home_blocks, only: %i(index)
+
+  def home_block_repository
+    @home_block_repository ||= HomeBlockRepository.new
+  end
+
   def index
     @highlights = GridHighlight.order('id ASC').all
   end
@@ -6,4 +12,9 @@ class CyclesController < ApplicationController
   def show
     @cycle = Cycle.find params[:id]
   end
+
+  def home_blocks
+    @home_blocks ||= home_block_repository.blocks
+  end
+  helper_method :home_blocks
 end
