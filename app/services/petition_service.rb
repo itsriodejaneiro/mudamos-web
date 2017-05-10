9692cb1a66c49e31450e6a222a4065819e4b33f4
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-05-10 14:03:56 -0300

    Added new info to the petition service

--- app/services/petition_service.rb
+++ app/services/petition_service.rb
@@ -11,12 +11,40 @@ class PetitionService
     @petition_repository = petition_repository
   end
 
-  def fetch_petition_info(petition_id, fresh: false)
-    cache_key = "mobile_petition_info:#{petition_id}"
+  PetitionInfo = Struct.new(
+    :updated_at,
+    :signatures_count,
+    :blockchain_address,
+    :initial_signatures_goal,
+    :total_signatures_required,
+    :current_signatures_goal,
+  )
+  def fetch_petition_info_with(petition:, fresh: false)
+    cache_key = "mobile_petition_info:v2:#{petition.id}"
 
-    Rails.cache.fetch(cache_key, force: fresh) do
-      mobile_service.petition_info(petition_id)
+    info = Rails.cache.fetch(cache_key, force: fresh) do
+      mobile_service.petition_info(petition.id)
     end
+
+    current_signatures_goal = compute_current_signatures_goal(
+      signatures_count: info.signatures_count,
+      initial_signatures_goal: petition.initial_signatures_goal,
+      total_signatures_required: petition.signatures_required
+    )
+
+    PetitionInfo.new(
+      info.updated_at,
+      info.signatures_count,
+      info.blockchain_address,
+      petition.initial_signatures_goal,
+      petition.signatures_required,
+      current_signatures_goal
+    )
+  end
+
+  def fetch_petition_info(petition_id, fresh: false)
+    petition = petition_repository.find_by_id!(petition_id)
+    fetch_petition_info_with petition: petition, fresh: fresh
   end
 
   def fetch_petition_signers(petition_id, limit, fresh: false)
@@ -58,5 +86,12 @@ class PetitionService
         Array.new
       end
     end
+  end
+
+  def compute_current_signatures_goal(signatures_count:, initial_signatures_goal:, total_signatures_required:)
+    signatures_count ||= 0
+    current_signatures_goal = ((signatures_count / initial_signatures_goal) + 1) * initial_signatures_goal
+
+    [current_signatures_goal, total_signatures_required].min
   end
 end
