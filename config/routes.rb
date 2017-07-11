75dcd1b349701368213cc04dc4c92a2eed037337
Lucas Pélos <lucas.pelos@gmail.com>
2017-07-11 17:19:01 -0300

    Css changes and index to root in static page

--- config/routes.rb
+++ config/routes.rb
@@ -157,11 +157,9 @@ Rails.application.routes.draw do
 
   match '/ping', to: 'ping#show', via: :get
 
-  match "/index", to: "static#index", via: :get
-
   match "/quem-somos", to: "static#about", via: :get
 
-  root "cycles#index"
+  root "static#index"
 
   resources :cycles, only: [:show], path: 'temas' do
     cycle_routes
