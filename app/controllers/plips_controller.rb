1d9013c87e490fea9a7f4740893022d68975b06b
Cassiano D'Andrea <cassiano@tagview.com.br>
2017-08-03 12:36:36 -0300

    Desligando layout no `PlipsController`

--- app/controllers/plips_controller.rb
+++ app/controllers/plips_controller.rb
@@ -1,4 +1,6 @@
 class PlipsController < ApplicationController
+  layout false
+
   def index
     plip_repository = PlipRepository.new
     @plips = plip_repository.all_initiated(filters: { scope: params[:scope_coverage] || "all" })
