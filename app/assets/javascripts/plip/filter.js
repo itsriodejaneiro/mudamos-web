5eab98b0cf6410ee15f8b62804d5be283a0af56b
Lucas Draichi <lucasdraichi@gmail.com>
2017-08-03 13:00:39 -0300

    removendo código inutil

--- app/assets/javascripts/plip/filter.js
+++ app/assets/javascripts/plip/filter.js
@@ -1,13 +1,3 @@
-var VIEWPORT_WIDTH = $(window).width();
-
-    if( VIEWPORT_WIDTH > 600 ){
-        var ROWS = 1;
-        var SLIDES = 3;
-    } else if( VIEWPORT_WIDTH < 559 ){
-        var ROWS = 1;
-        var SLIDES = 1;
-    }
-
 $(document).ready(function() {
 
     $('#btn-quemsomos').hover(
