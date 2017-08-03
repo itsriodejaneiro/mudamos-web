aff53011383f69992b7783982691717ca1a8ea9b
Lucas Draichi <lucasdraichi@gmail.com>
2017-08-03 16:40:18 -0300

    icone dos cards municipais e estaduais ajuastados

--- app/helpers/plips_helper.rb
+++ app/helpers/plips_helper.rb
@@ -6,7 +6,7 @@ module PlipsHelper
   
   def scope_name(plip_detail)  
     if plip_detail.citywide?
-      "#{plip_detail.city.name} - #{plip_detail.city.uf}"   
+      "#{plip_detail.city.name}-#{plip_detail.city.uf}"   
     elsif plip_detail.statewide?     
       plip_detail.uf  
     end  
