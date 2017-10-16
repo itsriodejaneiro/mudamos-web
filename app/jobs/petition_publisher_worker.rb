831ca9365cf7ebe2252843d90d49c5af54ce98b9
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-10-16 13:51:57 -0200

    Do not send push message when creating a new plip

--- app/jobs/petition_publisher_worker.rb
+++ app/jobs/petition_publisher_worker.rb
@@ -4,16 +4,13 @@ class PetitionPublisherWorker
 
   attr_reader :repository
   attr_accessor :petition_service
-  attr_accessor :notifier
 
   def initialize(
     repository: PetitionPlugin::DetailVersionRepository.new,
-    petition_service: PetitionService.new,
-    notifier: PetitionNotifierWorker
+    petition_service: PetitionService.new
   )
     @repository = repository
     @petition_service = petition_service
-    @notifier = notifier
   end
 
   def perform(sqs_msg, body)
@@ -26,23 +23,9 @@ class PetitionPublisherWorker
       Rails.logger.info "Version published #{petition_detail_version_id}"
 
       refresh_caches version
-
-      if should_notify_users? version
-        notifier.perform_async id: version.id
-      else
-        Rails.logger.info "Skipping push message."
-      end
     else
       Rails.logger.warn "Version not found #{petition_detail_version_id}"
     end
-  end
-
-  def should_notify_users?(version)
-    is_first_version(version)
-  end
-
-  def is_first_version(version)
-    version.petition_plugin_detail.petition_detail_versions.count == 1
   end
 
   def refresh_caches(version)
