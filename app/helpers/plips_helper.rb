c1c2c68afcea20911b1cd3de39fa12245dac0ca5
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-10-19 11:17:46 -0200

    Add national causes tab

--- app/helpers/plips_helper.rb
+++ app/helpers/plips_helper.rb
@@ -1,14 +1,16 @@
 module PlipsHelper
-  # def display_scope_coverage(scope_coverage)
-  #   dic = { citywide: "Municipal", statewide: "Estadual", nationwide: "Federal" }
-  #   dic[scope_coverage.to_sym]
-  # end
-  
-  def scope_name(plip_detail)  
+  def plip_scope_description(plip_detail)
+    return if plip_detail.nationwide?
+    return t :national_cause if plip_detail.city.blank? && plip_detail.uf.blank?
+
     if plip_detail.citywide?
-      "#{plip_detail.city.name}-#{plip_detail.city.uf}"   
-    elsif plip_detail.statewide?     
-      plip_detail.uf  
-    end  
+      "#{plip_detail.city.name}-#{plip_detail.city.uf}"
+    else
+      plip_detail.uf
+    end
+  end
+
+  def include_plip_pin?(plip_detail)
+    [PetitionPlugin::Detail::STATEWIDE_SCOPE, PetitionPlugin::Detail::CITYWIDE_SCOPE].include? plip_detail.scope_coverage
   end
 end
