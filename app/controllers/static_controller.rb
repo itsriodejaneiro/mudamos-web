f0af577eeb80ef129cebaaa6d801b5b4fafe2ec0
Tagview <tagview@Tagviews-MacBook-Pro.local>
2018-05-15 14:58:47 -0300

    Add mobilize static page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,10 +3,14 @@ class StaticController < ApplicationController
 
   caches_action :index, expires_in: 30.minutes
   caches_action :about, expires_in: 1.hour
+  caches_action :mobilize, expires_in: 1.hour
 
   def about
   end
 
   def index
+  end
+
+  def mobilize
   end
 end
