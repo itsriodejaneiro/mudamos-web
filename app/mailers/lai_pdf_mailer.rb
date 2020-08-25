93279acf7165cd9dbf77b13cafae029b8fae4a4b
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2020-08-25 18:32:25 -0300

    Add LAI worker mailer

---
+++ app/mailers/lai_pdf_mailer.rb
@@ -0,0 +1,16 @@
+class LaiPdfMailer < ActionMailer::Base
+  default from: "contato@mudamos.org",
+          reply_to: "contato@mudamos.org"
+
+  def send_mail(pdf_url, lai)
+    @name = lai["name"]
+    @pdf_url = pdf_url
+
+    mail(
+      to: lai["email"],
+      subject: "Regulamentação da Lei do Acesso à Informação de #{lai["city"]}",
+      template_path: 'mailers/notification_mailer',
+      template_name: 'lai_email_template'
+    )
+  end
+end
