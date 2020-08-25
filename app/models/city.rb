f75417410df544d402684f335ba162b0b97aab43
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2020-08-25 18:09:20 -0300

    Add city finder matching name and uf

--- app/models/city.rb
+++ app/models/city.rb
@@ -12,4 +12,14 @@
 
 class City < ActiveRecord::Base
   default_scope { order('name ASC') }
+
+  def self.find_best_match(searched_city, searched_uf = nil)
+    cities = searched_uf ? City.where(uf: searched_uf) : City.all
+    ranks = searched_city.squeeze.strip.downcase.no_accent.pair_distance_similar(
+      cities.map { |city| city.name.downcase.no_accent }
+    )
+    max_rank = ranks.max
+    best_match_index = ranks.index { |r| r == max_rank }
+    cities[best_match_index]
+  end
 end
