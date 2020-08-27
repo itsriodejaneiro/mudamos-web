a48b96be25ae1e4f3fcb691db5902fd1e30c53b5
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2020-08-26 21:59:02 -0300

    Remove unecessary code

--- app/repositories/lai_pdf_repository.rb
+++ app/repositories/lai_pdf_repository.rb
@@ -1,6 +1,5 @@
 class LaiPdfRepository
   include Repository
-  include UserInput
 
   def find(id)
     LaiPdf.find_by_id id
--- spec/factories/lai_pdfs.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :lai_pdf do
-    
-  end
-
-end
