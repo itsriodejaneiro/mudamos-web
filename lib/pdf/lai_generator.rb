743f1265d737288ccb0cd1a8f60014a916f45d31
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2020-08-28 11:52:46 -0300

    Add debug code

--- lib/pdf/lai_generator.rb
+++ lib/pdf/lai_generator.rb
@@ -10,14 +10,17 @@ class Pdf::LaiGenerator
     result = lai_pdf << justification_page
     result.new_page
 
+    result.save Rails.root.join('tmp', 'result.pdf')
+    justification_page.save Rails.root.join('tmp', 'justification.pdf')
+
     result.to_pdf
   end
 
   private
 
   def fill_gaps(template, city_name)
-    template.pages[0].textbox "#{city_name.no_accent}.", height: 11, width: -1, y: 715, x: 326.5, font_size: 11, text_align: :left
-    template.pages[0].textbox "#{city_name.no_accent};", height: 11, width: -1, y: 490, x: 148, font_size: 11, text_align: :left
+    template.pages[0].textbox "#{city_name}.", height: 11, width: -1, y: 715, x: 326.5, font_size: 11, text_align: :left
+    template.pages[0].textbox "#{city_name};", height: 11, width: -1, y: 490, x: 148, font_size: 11, text_align: :left
 
     template
   end
@@ -40,7 +43,7 @@ class Pdf::LaiGenerator
   def build_default_justification(city_name)
     template = CombinePDF.load(Rails.root.join('app', 'assets', 'docs', 'lai_template_default_justification.pdf'))
 
-    template.pages[0].textbox "#{city_name.no_accent}", height: 11, width: -1, y: 565, x: 232, font_size: 11, text_align: :left
+    template.pages[0].textbox "#{city_name}", height: 11, width: -1, y: 565, x: 232, font_size: 11, text_align: :left
 
     template
   end
