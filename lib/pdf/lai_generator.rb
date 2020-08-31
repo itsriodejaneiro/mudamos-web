773c291ebb609c8bd057f9fa713cf4fd0552f108
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2020-08-31 16:54:15 -0300

    Fix lai pdf city name with accents

--- lib/pdf/lai_generator.rb
+++ lib/pdf/lai_generator.rb
@@ -1,25 +1,31 @@
 class Pdf::LaiGenerator
   def generate(city_name:, is_big_city:, justification: nil)
-    template = is_big_city ? 'lai_template_big_cities.pdf' : 'lai_template_small_cities.pdf'
-
-    lai_file_template = CombinePDF.load(Rails.root.join('app', 'assets', 'docs', template))
-
-    lai_pdf = fill_lai_gaps lai_file_template, city_name
+    lai_doc = build_lai_doc city_name, is_big_city
     justification_page = build_justification justification, city_name
 
-    result = lai_pdf << justification_page
+    result = lai_doc << justification_page
     result.new_page [0, 0, 595, 842] # A4 mediabox
+
+    justification_page.save Rails.root.join('tmp', 'justification.pdf')
+    result.save Rails.root.join('tmp', 'result.pdf')
 
     result.to_pdf
   end
 
   private
 
-  def fill_lai_gaps(template, city_name)
-    template.pages[0].textbox "#{city_name.no_accent}.", height: 11, width: -1, y: 715, x: 326.5, font_size: 11, text_align: :left
-    template.pages[0].textbox "#{city_name.no_accent};", height: 11, width: -1, y: 490, x: 148, font_size: 11, text_align: :left
+  def build_lai_doc(city_name, is_big_city)
+    template_file = is_big_city ? 'lai_template_big_cities.pdf' : 'lai_template_small_cities.pdf'
+    lai_with_gaps_pdf = CombinePDF.load(Rails.root.join('app', 'assets', 'docs', template_file))
 
-    template
+    lai_with_blocks = Prawn::Document.new({ page_size: "A4", page_layout: :portrait, margin: [80, 75] }) do |pdf|
+      pdf.draw_text "#{city_name}.", size: 11, at: [278, 635.5]
+      pdf.draw_text "#{city_name}.", size: 11, at: [118, 410.5]
+    end
+    lai_with_blocks_pdf = CombinePDF.parse(lai_with_blocks.render)
+
+    lai_with_gaps_pdf.pages[0] << lai_with_blocks_pdf.pages[0]
+    lai_with_gaps_pdf
   end
 
   def build_justification(justification, city_name)
@@ -33,15 +39,18 @@ class Pdf::LaiGenerator
       end
     end
 
-    justification_result = justification.render
-    CombinePDF.parse(justification_result)
+    CombinePDF.parse(justification.render)
   end
 
   def build_default_justification(city_name)
-    template = CombinePDF.load(Rails.root.join('app', 'assets', 'docs', 'lai_template_default_justification.pdf'))
+    justification_with_gaps_pdf = CombinePDF.load(Rails.root.join('app', 'assets', 'docs', 'lai_template_default_justification.pdf'))
 
-    template.pages[0].textbox "#{city_name.no_accent}", height: 11, width: -1, y: 565, x: 232, font_size: 11, text_align: :left
+    justification_with_blocks = Prawn::Document.new({ page_size: "A4", page_layout: :portrait, margin: [80, 75] }) do |pdf|
+      pdf.draw_text city_name, size: 11, at: [194, 485]
+    end
+    justification_with_blocks_pdf = CombinePDF.parse(justification_with_blocks.render)
 
-    template
+    justification_with_gaps_pdf.pages[0] << justification_with_blocks_pdf.pages[0]
+    justification_with_gaps_pdf
   end
 end
